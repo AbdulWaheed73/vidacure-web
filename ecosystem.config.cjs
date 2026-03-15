5c5d63794fd5f2c6342f0c9f6f7ea5f05a8a5d62
AbdulWaheed73 <aw736024@gmail.com>
2026-03-15 16:47:13 +0500

    ecossytem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'vidacure-web',
     script: 'serve',
-    args: '-s dist -l 5173',
+    args: '-s dist -l tcp://0.0.0.0:5173',
     env_production: {
       NODE_ENV: 'production'
     },
