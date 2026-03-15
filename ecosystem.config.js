88a7db488d3edde743788c4762173e5bee352696
AbdulWaheed73 <aw736024@gmail.com>
2026-03-15 16:40:01 +0500

    ecosystem configs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,11 @@
-// ecosystem.config.js
-export const apps = [{
-  name: 'vidacure-web',
-  script: 'serve',
-  args: '-s dist -l 5173',
-  env_production: {
-    NODE_ENV: 'production'
-  }
-}];
+module.exports = {
+  apps: [{
+    name: 'vidacure-web',
+    script: 'serve',
+    args: '-s dist -l 5173',
+    env_production: {
+      NODE_ENV: 'production'
+    },
+    time: true
+  }]
+};
